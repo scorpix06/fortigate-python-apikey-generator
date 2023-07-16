d432234a8995ad9b46121a792e0060fe27e10fbe
scorpix06 <alban@cipre.com>
2023-07-16 19:51:54 +0200

    Translete main.py in english

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ ip = "192.168.1.99"
 user = "admin"
 password = ""
 portSSH = 22
-newPassword = "NOUVEAUMOTDEPASSE"
+newPassword = "NEWPASSWORD"
 
-httpServer = "192.168.40.112" # Adresse IP du serveur web contenant les fichier d'OS
+httpServer = "192.168.40.112" # IP address or FQDN of the webserver containing the images
 
 apiProfileName = "Super_Admin_API"
 apiUserName = "RTS_Api"
@@ -17,6 +17,7 @@ apiUserName = "RTS_Api"
 
 config = fortipy(ip, user, password, portSSH)
 config.fisrtInit(newPassword)
+config.createApiToken(apiUserName, apiProfileName)
 config.upgrade(httpServer)
 
 
